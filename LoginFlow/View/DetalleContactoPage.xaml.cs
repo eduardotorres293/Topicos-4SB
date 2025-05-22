d2a81bc3f1c7b25faa31f004f1d865baba11ea41
pc <al23760375@ite.edu.mx>
2025-05-21 21:53:51 -0700

    - Cambios varios    * Se terminó de adaptar el LoginFlow       ° Ocurrian errores con la inserción del git debido al límite de tamaño presente en GitHub

--- LoginFlow/View/DetalleContactoPage.xaml.cs
+++ LoginFlow/View/DetalleContactoPage.xaml.cs
@@ -1,16 +1,22 @@
 using Practica5.ViewModel;
+using Practica5.Model;
 
 namespace Practica5.View;
 
+[QueryProperty(nameof(Contacto), "Contacto")]
 public partial class DetalleContactoPage : ContentPage
 {
-	public DetalleContactoPage()
-	{
-		InitializeComponent();
-        BindingContext = new DetalleContactoVM();
-    }
-    private async void OnVolverClicked(object sender, EventArgs e)
+    private readonly DetalleContactoVM _viewModel;
+    public Contacto Contacto
     {
-        await Shell.Current.GoToAsync("..");
+        get => _viewModel.Contacto!;
+        set => _viewModel.Contacto = value;
+    }
+
+    public DetalleContactoPage()
+    {
+        InitializeComponent();
+        _viewModel = new DetalleContactoVM();
+        BindingContext = _viewModel;
     }
 }
